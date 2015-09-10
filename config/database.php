8b21086320b4033c69c89ad40937bfbb5d25cac2
Nick Chikovani <nikabika97@gmail.com>
2015-09-10 23:57:18 +0400

    fixed .env bug

--- config/database.php
+++ config/database.php
@@ -1,18 +1,1 @@
 <?php
-use Illuminate\Database\Capsule\Manager as Capsule;
-
-$capsule = new Capsule;
-
-$capsule->addConnection([
-	'driver' 	=> 'mysql',
-	'host'		=> getenv('DB_HOST'),
-	'username'	=> getenv('DB_USER'),
-	'password'	=> getenv('DB_PASS'),
-	'database'	=> getenv('DB_NAME'),
-	'charset'	=> 'utf8',
-	'collation'	=> 'utf8_unicode_ci',
-]);
-
-$capsule->setAsGlobal();
-
-$capsule->bootEloquent();
--- start.php
+++ start.php
@@ -1,8 +1,9 @@
 <?php
 
 require __DIR__.'/vendor/autoload.php';
-require __DIR__.'/config/database.php';
-require __DIR__.'/helpers.php';
+
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
@@ -10,10 +11,10 @@ $capsule = new Capsule;
 
 $capsule->addConnection([
 	'driver' 	=> 'mysql',
-	'host'		=> 'localhost',
-	'username'	=> 'homestead',
-	'password'	=> 'secret',
-	'database'	=> 'DomainMetricsChecker',
+	'host'		=> getenv('DB_HOST'),
+	'username'	=> getenv('DB_USER'),
+	'password'	=> getenv('DB_PASS'),
+	'database'	=> getenv('DB_NAME'),
 	'charset'	=> 'utf8',
 	'collation'	=> 'utf8_unicode_ci',
 ]);
@@ -21,3 +22,5 @@ $capsule->addConnection([
 $capsule->setAsGlobal();
 
 $capsule->bootEloquent();
+
+require __DIR__.'/helpers.php';
