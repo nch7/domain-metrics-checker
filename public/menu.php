201777950fbc149bfe8528c5b4f2110e0754ea83
Nick Chikovani <nikabika97@gmail.com>
2015-09-11 08:36:42 +0400

    changed name

--- public/menu.php
+++ public/menu.php
@@ -7,7 +7,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="#">Yashar's Private Majestic Tool</a>
+			<a class="navbar-brand" href="#">Private Majestic Tool</a>
 		</div>
 		<div id="navbar" class="collapse navbar-collapse">
 			<ul class="nav navbar-nav">
